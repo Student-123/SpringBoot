cae54277e2a51f1f78bc955f5eb36e80cf36dac1
Student123 <studentnumber123@gmail.com>
2016-08-22 09:10:04 +0530

    Stubbing works

--- SoapMocker/src/main/java/com/workplace/controllers/HandleSoapRequest.java
+++ SoapMocker/src/main/java/com/workplace/controllers/HandleSoapRequest.java
@@ -4,14 +4,44 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  * Created by Santhosh on 8/21/2016.
  */
 @RestController
 @RequestMapping("/")
 public class HandleSoapRequest {
-    @RequestMapping(method = RequestMethod.GET)
-    public String sayHello(){
-        return "Hello";
+    @RequestMapping(method = RequestMethod.POST)
+    public String sayHello()
+    {
+        return getFile("SoapResponse.txt");
+    }
+
+    private String getFile(String fileName) {
+
+        StringBuilder result = new StringBuilder("");
+
+        //Get file from resources folder
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource(fileName).getFile());
+
+        try (Scanner scanner = new Scanner(file)) {
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                result.append(line).append("\n");
+            }
+
+            scanner.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result.toString();
+
     }
 }
